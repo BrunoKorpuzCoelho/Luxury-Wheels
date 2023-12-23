d2c495adbe75dcf17c5b77333bdeefce672fbb48
Bruno Coelho <138626948+BrunoKorpuzCoelho@users.noreply.github.com>
2023-12-23 18:37:36 +0000

    Import Sqlite

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__) # Server
 app.config['SQLALCHEMY_DATABASE_URI'] = "C://Users//KorpuZ//Desktop//Projetofinal//database//database.db" # Path to the database
