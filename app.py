39324947406d8b6c98451ac0855f9572ef578521
Bruno Coelho <138626948+BrunoKorpuzCoelho@users.noreply.github.com>
2023-12-23 18:11:09 +0000

    Data base and cursor creation, html syntax creation

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) # Server
+app.config['SQLALCHEMY_DATABASE_URI'] = "C://Users//KorpuZ//Desktop//Projetofinal//database//database.db" # Path to the database
+db = SQLAlchemy(app) # Cursor
+
